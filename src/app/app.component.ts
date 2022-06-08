18e61de32688858125586b10dbc807baf0a53b1d
STiwari <71030367+Stiwari28@users.noreply.github.com>
2022-06-08 13:36:32 +0530

    Adding First code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first_angular_project';
+  title = 'Welcome to Creator World ';
 }
